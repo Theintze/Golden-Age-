c1f3762370c587a25831a8c6ae7ecd52d9eb718a
Mouchamed Ekmel Intze <36741001+Theintze@users.noreply.github.com>
2019-06-16 21:32:11 +0100

    Importing the assets
    These are the main assets of the game. It includes the Audio files, Background images, Fonts used, Scenes of the game, C# scripts, and the textures.

---
+++ Assets/Scripts/CursorOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorOff : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        Screen.lockCursor = false;
+        
+    }
+}
